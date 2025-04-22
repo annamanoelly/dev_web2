53a760e2f4dbd2972548584695d5227284c6fb24
annamanoelly <anna.mannuuh73@gmail.com>
2025-04-22 20:06:15 -0300

    introdução a js

--- estruturas _prog/script.js
+++ estruturas _prog/script.js
@@ -7,3 +7,23 @@
 //var nome = "Miranda";
 //const ip = "127.0.0.1";
 
+//let idade = prompt("Qual é a sua idade?")
+//console.log(idade)
+
+//let nome = prompt("Qual é o seu nome?")
+//console.log(nome)
+
+//função built-in:alert
+//alert("Se inscreva no meu canal")
+//alert("Tem certeza que já se inscreveu?")
+
+//built-in:math.x()
+let maiorNumero = Math.max(1,2,3);
+console.log(maiorNumero)
+
+
+let menorNumero = Math.min(1,2,3);
+console.log(menorNumero)
+
+let arredondarParaCima = Math.ceil(3.478989889);
+console.log(arredondarParaCima)
