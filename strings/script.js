6a692d22def6daf8a1dca82352825c7cfbaedda9
annamanoelly <anna.mannuuh73@gmail.com>
2025-04-29 19:05:18 -0300

    string e numeros

---
+++ strings/script.js
@@ -0,0 +1,11 @@
+console.log(typeof "Isso é uma String");
+console.log(typeof "Esse texto também");
+console.log(typeof "E este também");
+console.log(typeof "Oi eu sou a Maria")
+console.log(typeof "Eu queria ir no show do bts")
+console.log(typeof "Teste")
+console.log(typeof "Infinity")
+console.log(typeof Infinity)
+
+console.log("Essa é uma string \n de duas linhas")
+console.log(`a soma de 2+2 é ${2+2}`)
