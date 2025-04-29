16dd3e3b61aa17ef5a9d3f18ef1161ee56e64323
annamanoelly <anna.mannuuh73@gmail.com>
2025-04-29 19:39:06 -0300

    switch

---
+++ break/script.js
@@ -0,0 +1,49 @@
+//break
+
+/*for (let i = 5; i < 20; i = i + 1){
+    if(i % 10==0){
+        console.log("Saiu do loop");
+        break
+    }
+    console.log("Prosseguindo o loop");
+}
+
+
+//continue
+let x =0
+while(x<11){
+    x=x+1;
+    if (x%2==0){
+        continue;
+    }
+    console.log(x)
+    x=x+1;
+}
+
+
+for (let i =10;>0;i=i-1){
+    if(i%2==0){
+        console.log("Cai no continue");
+        continue;
+    }
+    console.log(i);
+}
+
+
+for(let numero =0;numero < 10; numero +-1){
+    console.log("O numero é:"+numero);
+}
+
+
+
+let num=5;
+while(num<50){
+    console.log(num)
+    num+=10;
+}
+
+
+for(let i =0 ; i < 10 ; i++){
+    console.log("O i é" + i);
+}
+*/
---
+++ switch/script.js
@@ -0,0 +1,28 @@
+/*let nome = "João" 
+switch(nome){
+    case "João":
+        console.log("o seu nome é João")
+        break
+    case "Marcos":
+        console.log("o seu nome é Marcos")
+        break
+    default:
+        console.log("O seu nome não é joão e nem marcos!")
+        break
+}
+*/
+
+let nome = "Victor" 
+switch(nome){
+    case "Victor":
+        console.log("o seu nome é Victor")
+        break
+    case "Joanito":
+        console.log("o seu nome é Juan")
+        break
+    default:
+        console.log("nome não existente")
+        break
+}
+
+
