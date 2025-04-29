f674723f0cca9c73d9339e16be349d4c3eb784d0
annamanoelly <anna.mannuuh73@gmail.com>
2025-04-29 20:29:05 -0300

    exercicios_2

---
+++ exercicios_2/script.js
@@ -0,0 +1,111 @@
+/*1.Armazene em valores em variáveis com cada um dos tipos de dados vistos;
+String, Number e Boolean;
+let nome = "Maria"
+let idade = 5
+let estouEstudando = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof estouEstudando);
+*/
+
+/*Cria uma estrutura if que verifica a entrada na balada, se tiver mais de 18
+anos pode entrar;
+● Armazenar a idade em uma variável com let;
+● Insira uma instrução console.log(“Pode entrar”), caso a pessoa tenha mais
+que 18 anos; 
+
+let idade = "16"
+if (idade >= 18) {
+    console.log("Pode entrar");
+}
+*/
+
+/*Crie uma constante com o seu nome como valor;
+● Depois uma estrutura if que verifica se o seu nome é o que está na
+constante;
+● Se estiver, emita uma mensagem de saudação com console.log(); 
+const nome = "Anna"
+if (nome == "Anna"){
+    console.log("Seja bem vinda")
+}
+*/
+
+/*A função Math.pow() exibe a potencia de um número;
+● Teste a função com console.log() e as seguintes bases: 2, 3, 18 e o
+expoente deve ser 2;
+● Recebe 2 argumentos, base e expoente;
+● Ex: Math.pow(5,3); 
+const num = 18
+console.log(Math.pow(2, 2));   
+console.log(Math.pow(3, 2));   
+console.log(Math.pow(num, 2));   */
+
+/*Armazene a velocidade de um carro em uma variável, com o número que
+desejar;
+● Faça uma estrutura if/else que verifica se ele está acima da
+velocidade;
+● 80 é a velocidade máxima permitida;
+● Se estiver acima ou abaixo exiba mensagens com console.log 
+let velocidade = 55
+if (velocidade > 80){
+    console.log("Acima da velocidade, levou multa")
+} else{
+    console.log("abaixo da velocidade")
+}*/
+
+/*Faça uma estrutura if/else para verificar se um usuário pode dirigir;
+● Armazene em variáveis algumas informações sobre o usuário: idade, se tem
+CNH
+● Se a idade for maior que 18 e não possuir CNH, exiba uma
+mensagem;
+● Se a idade for maior que 18 e tem CNH, exiba uma mensagem;
+● Se não tiver 18 nem CNH, exiba outra mensagem; 
+
+let idade = 22
+let possuiCNH = true;
+
+// Estrutura condicional
+if (idade >= 18 && possuiCNH) {
+  console.log("Você pode dirigir.");
+} else if (idade >= 18 && !possuiCNH) {
+  console.log("Você tem idade para dirigir, mas precisa tirar a CNH.");
+} else {
+  console.log("Você não tem idade nem CNH para dirigir.");
+}*/
+
+/*Escreva um loop while que exibe números de 0 a 10 no console;
+let numero = 0;
+
+while (numero <= 10) {
+  console.log(numero);
+  numero++;
+}*/
+
+/*Escreva um loop for que exibe números de 100 a 50 no console; 
+let numero = 50;
+
+while (numero <= 100) {
+  console.log(numero);
+  numero++;
+}*/
+
+/*Escreva um loop for ou while que exiba qual número é par e qual
+número é ímpar,
+● O contador deve iniciar em 0 e ir até 50; 
+for (let i = 0; i <= 50; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " é par");
+    } else {
+      console.log(i + " é ímpar");
+    }  
+}*/
+
+/*Verifique se o número é primo!
+● Um número primo, é um número natural, maior que 1 e apenas
+divisível por si próprio e por 1; */
+
+  
+
+
+
